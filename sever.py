70502e6b1af51b3de05a80bee77c540ed284bd43
DevonEvant <horningch@gmail.com>
2020-12-20 08:51:01 +0800

    S

--- sever.py
+++ sever.py
@@ -10,7 +10,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return ("<p>123</p>")
+    return ("<p>home</p>")
 
+@app.route('/test')
+def home():
+    return ("<p>test</p>")
 
-app.run()
+if __name__=="__main__":
+    app.run()
